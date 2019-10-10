9f0082b6f31568cb16c1cb9f128fdf3df899c3d5
JakubPilkowski <pilkowskijakub@gmail.com>
2019-10-10 14:23:04 +0200

    Ukończono zad nr 3

---
+++ zad3.py
@@ -0,0 +1,14 @@
+person = {'first': 'Jean-Luc', 'last': 'Picard'}
+print('{p[first]} {p[last]}'.format(p=person))
+
+data = [4, 8, 15, 16, 23, 42]
+print('{d[4]} {d[5]}'.format(d=data))
+
+print('{:.{prec}} = {:.{prec}f}'.format('Gibberish', 2.7182, prec=3))
+
+from datetime import datetime
+dt = datetime(2001, 2, 3, 4, 5)
+print('{:{dfmt} {tfmt}}'.format(dt, dfmt='%Y-%m-%d', tfmt='%H:%M'))
+
+print('{:{}{}{}.{}}'.format(2.7182818284, '>', '+', 10, 3))
+
